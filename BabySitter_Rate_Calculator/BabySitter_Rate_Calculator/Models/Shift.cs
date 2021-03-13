30bf0262c973ec2065c50b0316b1ffb41995eecc
Wendi Magee <wendimagee@gmail.com>
2021-03-13 11:02:50 -0500

    Alright!! I got all the tests passed. The problem was the change over at midnight from one day to another. I was trying to just parse it before and wasn't getting the results I wanted. So, I tried the "AddHours" method instead of trying to just parse a string. Worked like a charm!

--- BabySitter_Rate_Calculator/BabySitter_Rate_Calculator/Models/Shift.cs
+++ BabySitter_Rate_Calculator/BabySitter_Rate_Calculator/Models/Shift.cs
@@ -8,8 +8,6 @@ namespace BabySitter_Rate_Calculator.Models
 {
     public class Shift
     {
-        [Range (typeof(DateTime), "05:00:00 PM", "11:59:59 PM",
-            ErrorMessage = "Value for Start Time must be between 5:00pm and 2:00am")]
         public DateTime StartTime { get; set; }
         public DateTime EndTime { get; set; }
         public double ShiftPay { get; set; }
@@ -19,12 +17,13 @@ namespace BabySitter_Rate_Calculator.Models
         public double Calculate(Shift shift)
         {
             double ShiftPay;
-            DateTime lateRate = DateTime.Parse("11:00:00 PM");
+            var shiftStartDate = shift.StartTime.Date;
+            DateTime elevenOclock = shiftStartDate.AddHours(23);
             if (shift.EndTime.Date > shift.StartTime.Date)
             {
-                TimeSpan lateShiftLength = shift.EndTime.Subtract(lateRate);
+                TimeSpan lateShiftLength = shift.EndTime.Subtract(elevenOclock);
                 double latePay = lateShiftLength.TotalHours * 20;
-                TimeSpan earlyShiftLength = lateRate.Subtract(shift.StartTime);
+                TimeSpan earlyShiftLength = elevenOclock.Subtract(shift.StartTime);
                 double earlyPay = earlyShiftLength.TotalHours * 15;
                 ShiftPay = earlyPay + latePay;
                 return ShiftPay;
